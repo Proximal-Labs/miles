b6fc6d3a95604efd9678a050774817af22822a4d
Tom <ch271828n@outlook.com>
2026-09-16 23:05:44 +0800

    Separate parsed configuration from mutable runtime state
    Move runtime ownership and startup inputs out of spec interface migration. Retain scoped eval configuration when retargeting runtime endpoints.

--- miles/ray/wiring.py
+++ miles/ray/wiring.py
@@ -4,7 +4,7 @@ import ray
 from ray.actor import ActorHandle
 
 from miles.ray.specs.entrypoint import compute_specs
-from miles.utils.args.runtime import AllConfig
+from miles.utils.args.runtime import AllConfig, OrchestratorConfig
 from miles.utils.workers.backend_capability import factory
 from miles.utils.workers.backend_capability.base import BackendCapability
 from miles.utils.workers.connection_config import build_static_conn_config
@@ -33,7 +33,11 @@ def get_backend_capability(args) -> BackendCapability:
     static_connections = args.static_connections
 
     # TODO: temporary hack to be removed in later ops
-    if isinstance(args, AllConfig) and ClusterBackend(args.cluster_backend) is ClusterBackend.KUBERNETES:
+    if (
+        not static_connections.static_conn_infos
+        and isinstance(args, (AllConfig, OrchestratorConfig))
+        and ClusterBackend(args.cluster_backend) is ClusterBackend.KUBERNETES
+    ):
         static_connections = build_static_conn_config(specs=compute_specs(args))
 
     return factory.get_backend_capability(
