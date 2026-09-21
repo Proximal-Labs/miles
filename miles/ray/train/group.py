79c258945f8b5b6164734f05e6ec9661a72d662e
Tom <ch271828n@outlook.com>
2026-09-21 22:04:00 +0800

    Gate the trainer controller's startup on --trainer-init-expected-num-cells
    The trainer controller waits for the cell count declared by --trainer-init-expected-num-cells, resolved per trainer in compute_trainer_args next to the trainer's other derived arguments, instead of recomputing it from the actor and critic node counts its config no longer carries. A declared value keeps the trainer's config unchanged while the deployment scales; without one, the count the trainer's GPUs are deployed as is filled in, like --init-expected-num-cells on the inference side.

--- miles/ray/train/group.py
+++ miles/ray/train/group.py
@@ -11,7 +11,6 @@ from miles.ray.specs.train import compute_trainer_pool_id
 from miles.ray.train.cell import TrainerCell
 from miles.ray.train.cell_monitor import create_trainer_cell_health_checker
 from miles.utils import object_store
-from miles.utils.args.trainer_utils import compute_trainer_total_gpus
 from miles.utils.async_utils import AsyncioGatherUtils, gather_and_raise_first
 from miles.utils.audit_utils.event_analyzer import analyzer as event_analyzer
 from miles.utils.audit_utils.event_logger.logger import get_event_logger, is_event_logger_initialized
@@ -29,7 +28,6 @@ from miles.utils.ft_utils.health_checker import ActivenessTracker, NoopHealthChe
 from miles.utils.ft_utils.indep_dp import IndepDPInfo, create_tcp_store
 from miles.utils.init_once import InitOnce, init_once
 from miles.utils.logging_utils import configure_logger
-from miles.utils.megatron_args_utils import compute_trainer_num_cells
 from miles.utils.retry_utils import NonRetryableError, retry, retry_until_deadline
 from miles.utils.test_utils.ft_test_actions import FTTestActionControllerExecutor
 from miles.utils.tracking_utils.structured_log import log_structured
@@ -93,7 +91,7 @@ class TrainerController:
 
     @property
     def _expected_num_cells(self) -> int:
-        return compute_trainer_num_cells(self.args, total_gpus=compute_trainer_total_gpus(self.args, role=self._role))
+        return self.args.trainer_init_expected_num_cells
 
     @property
     def _cells(self) -> list[TrainerCell]:
